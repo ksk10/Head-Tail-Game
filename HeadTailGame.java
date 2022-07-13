234e29a930c82cfb23d87c388fc7663fc8ffae50
嶋貫幹大 <kanta1708191@shimanukikantanoMacBook-Air.local>
2022-07-13 14:16:14 +0900

    HoT: initial revision

---
+++ HeadTailGame.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class HeadTailGame{
+    public static void main(String args[]){
+        int head=0, tail=0;
+        Random rand = new Random();
+
+         System.out.println("Tossing a coin...");
+         for(int i=0; i<3; i++){
+            System.out.print("Round " + i + ": ");
+            int num = rand.nextInt(2);
+            if(num == 0){
+                System.out.println("Heads");
+                head++;
+            }
+            if(num == 1){
+                System.out.println("Tails");
+                tail++;
+            }
+        }
+        System.out.println("Heads: " + head + ", Tails: " + tail);
+    }
+}
