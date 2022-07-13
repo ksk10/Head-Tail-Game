a85131668fe822a1f34d87ee8569be0b222f3c7c
嶋貫幹大 <kanta1708191@shimanukikantanoMacBook-Air.local>
2022-07-13 14:23:12 +0900

    User name added

--- HeadTailGame.java
+++ HeadTailGame.java
@@ -1,9 +1,15 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class HeadTailGame{
     public static void main(String args[]){
         int head=0, tail=0;
         Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+
+	    System.out.println("Who are you?");
+	    String name = sc.nextLine();
+	    System.out.println("Hello, " + name + "!");
 
          System.out.println("Tossing a coin...");
          for(int i=0; i<3; i++){
